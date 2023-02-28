42395ef3b22da1f6d00f9a28571aa0ecf69334ad
djsereno <djsereno91@gmail.com>
2023-02-28 00:34:10 -0800

    Add border image styling

--- src/home.js
+++ src/home.js
@@ -20,15 +20,19 @@ function loadHomePage(parent) {
     'While working in commercial kitchens all around the Boston area, Chef Lorenzo held on to his ultimate dream of opening his own place. Every year for 25 years, he traveled to different regions of Mexico in order to study and expand his knowledge of local cooking techniques, ingredients and recipes. Over the years tasting food from all over Mexico and talking and practicing with cooks living there, including his mom and dad, aunts and uncles, Chef Lorenzo honed his culinary style to delicious perfection.',
     'Finally, after studying, sampling, and learning in-depth about all the different styles of regional Mexican cooking - and unable to deny his life-long dream any longer - he opened <strong>Rincon Mexicano</strong> in May 2014.',
     'Owned and operated by Chef Lorenzo Reyes along with his brother Ricardo, brother-in-law Andreas, and daughter Yvette, he brings all he has learned to <strong>Rincon Mexicano</strong> for your dining pleasure. ',
-    'Buen Provecho!',
   ];
   paragraphs.forEach((paraText) => {
     const para = document.createElement('p');
     para.innerHTML = paraText;
     aboutSection.appendChild(para);
   });
-
   content.appendChild(aboutSection);
+
+  const signOff = document.createElement('h2');
+  signOff.innerText = 'Buen Provecho!';
+  signOff.classList.add('plaque', 'orange');
+  content.appendChild(signOff);
+
   parent.appendChild(content);
 }
 
